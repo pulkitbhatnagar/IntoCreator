1c09e461fbcf99ce3de29fcd4fb7961ec36ee872
pulkitBhatnagar <ipaul@liveareacx.com>
2023-03-27 19:39:02 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import ListDisplay from "./Features/DisplayList";
 import InputForm from "./Features/Form";
 const App = () => {
   return (
-    <>
+    
       <HashRouter>
         <Switch>
           <Route exact path="/" component={InputForm} />
           <Route exact path="/list" component={ListDisplay} />
         </Switch>
       </HashRouter>
-    </>
+     
   );
 };
 
